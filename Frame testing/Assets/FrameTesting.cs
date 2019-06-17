640e79e51a65ba5c91e0bb86b87f534835bfc302
Toon <toonschaap3@gmail.com>
2019-06-17 12:31:21 +0200

    hoihoi

---
+++ Frame testing/Assets/FrameTesting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameTesting : MonoBehaviour
+{
+
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float StartTime = Time.realtimeSinceStartup;
+        transform.position += transform.forward * speed * Time.deltaTime;
+        float EndTime = Time.realtimeSinceStartup;
+        Debug.Log((EndTime - StartTime)*1000);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
